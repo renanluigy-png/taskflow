afe2732dc46e5aebaa7ef6fc44323aff0bd4671e
Luigy Renan <renanluigy@gmail.com>
2025-12-19 22:10:37 -0300

    Refactor task addition to include priority input

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
 const taskInput = document.getElementById("taskInput");
+const priorityInput = document.getElementById("priority");
 const addBtn = document.getElementById("addBtn");
 const taskList = document.getElementById("taskList");
 
-addBtn.addEventListener("click", () => {
-    const taskText = taskInput.value.trim();
+addBtn.addEventListener("click", addTask);
 
-    if (taskText === "") {
+function addTask() {
+    const text = taskInput.value.trim();
+    const priority = priorityInput.value;
+
+    if (text === "") {
         alert("Digite uma tarefa!");
         return;
     }
 
     const li = document.createElement("li");
-    li.textContent = taskText;
+
+    li.innerHTML = `
+        <span>${text}</span>
+        <span>[${priority}]</span>
+    `;
 
     taskList.appendChild(li);
     taskInput.value = "";
-});
+}
